1ece96cf7920715c4fa99edf441061071b90304e
santi2019 <santiagovietto5@hotmail.com>
2024-12-10 12:27:57 -0300

    arreglo geoserver upload

--- backend/src/scripts/geoserver_upload_v7.py
+++ backend/src/scripts/geoserver_upload_v7.py
@@ -59,7 +59,8 @@ def geoserver_upload():
             geo.set_default_workspace(workspace_name)
             print(f"El workspace '{workspace_name}' ha sido creado y establecido por defecto.")
             sleep_for_a_bit(20)
-    
+    sleep_for_a_bit(40)
+
     ###################################################################################################################################
 
     ## PASO 2: Proceso de creacion de capas y asignacion de estilos. 
@@ -75,10 +76,10 @@ def geoserver_upload():
 
     print(f"Creando coveragestore para la capa: {PTM_layer_name}")
     geo.create_coveragestore(layer_name=PTM_layer_name, path=PTM_tif_file, workspace=workspace_name)
-    sleep_for_a_bit(20)
+    sleep_for_a_bit(40)
     print(f"Publicando estilo para la capa: {PTM_layer_name}")
     geo.publish_style(layer_name=PTM_layer_name, style_name=PTM_style_name, workspace=workspace_name)
-    sleep_for_a_bit(20)
+    sleep_for_a_bit(40)
 
 
     spi_scales = ['1', '2', '3', '6', '9', '12', '24', '36', '48', '60', '72']
@@ -92,11 +93,11 @@ def geoserver_upload():
         
         print(f"Creando coveragestore para la capa: {SPI_layer_name}")
         geo.create_coveragestore(layer_name=SPI_layer_name, path=SPI_tif_file, workspace=workspace_name)
-        sleep_for_a_bit(20)
+        sleep_for_a_bit(40)
 
         print(f"Publicando estilo para la capa: {SPI_layer_name}")
         geo.publish_style(layer_name=SPI_layer_name, style_name=SPI_style_name, workspace=workspace_name)
-        sleep_for_a_bit(20)
+        sleep_for_a_bit(40)
     
 
 
